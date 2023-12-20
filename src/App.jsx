2ebc57b2cfacab56f9fdebb8e3a484ee4affb2de
Cooltor <romaindbus86@gmail.com>
2023-12-20 19:29:22 +0100

    react query setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Booking from "./pages/Bookings";
@@ -10,27 +13,37 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
-            <Route path="booking" element={<Booking />} />
+            <Route path="bookings" element={<Booking />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
           </Route>
-
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,21 @@
+import { getCabins } from "../services/apiCabins";
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://iekdghddmldcabtztgai.supabase.co/storage/v1/object/public/cabin-images/cabin-005.jpg"
+        alt="cabin"
+      />
     </Row>
   );
 }
