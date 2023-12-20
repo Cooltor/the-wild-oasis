06dd7811e9165388cba0efc671bf68ce4b5bac81
Cooltor <romaindbus86@gmail.com>
2023-12-20 14:04:53 +0100

    styled components themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: blue;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  margin: 20px;
+  box-shadow: var(--shadow--sm);
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("click")}>Check In</Button>
+        <Button onClick={() => alert("click")}>Check Out</Button>
+
+        <Input type="number" placeholder="Number of guest" />
+      </StyledApp>
     </>
   );
 }
